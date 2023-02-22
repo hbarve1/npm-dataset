0aceb711d27e266a339b5c5b0b504088880d8371
Himank Barve <hbarve1592@gmail.com>
2023-02-22 13:56:45 +0530

    .

--- npm-registry-analytics/apps/api/src/index.ts
+++ npm-registry-analytics/apps/api/src/index.ts
@@ -1,4 +1,6 @@
 import wait from '@hbarve1/wait';
+import { getHeapStatistics } from 'node:v8';
+import log from 'why-is-node-running';
 
 import {
   getAllPackageNamesFromFiles,
@@ -8,40 +10,54 @@ import {
 import { insertJson, newClientStub, newClient } from './dgraph';
 
 async function writeNpmPackageNamesToDatabaseSub(packageNames, i) {
+  // try {
   const dgraphClientStub = newClientStub();
   const dgraphClient = newClient(dgraphClientStub);
-  try {
-    const pkgJson = await getNpmPackage(packageNames[i]);
-    // const pkgJson = await getNpmPackage("--hiljson");
-    // console.log(i, packageNames[i]);
-    // console.dir(pkgJson, { depth: null });
-    // console.log(
-    //   'parsePackageJson fn calling ===================================='
-    // );
+  // try {
+  const pkgJson = await getNpmPackage(packageNames[i]).catch((error) => {
+    console.log('Error Inserting', i, packageNames[i]);
     // const parsedPkgJson = parsePackageJson(pkgJson);
     // console.dir(parsedPkgJson, { depth: null });
+    console.dir(error, { depth: null });
+    // console.error(error);
+  });
+  // const pkgJson = await getNpmPackage("--hiljson");
+  // console.log(i, packageNames[i]);
+  // console.dir(pkgJson, { depth: null });
+  // console.log(
+  //   'parsePackageJson fn calling ===================================='
+  // );
+  // const parsedPkgJson = parsePackageJson(pkgJson);
+  // console.dir(parsedPkgJson, { depth: null });
 
-    try {
-      const res = await insertJson(dgraphClient, {
-        _id: packageNames[i],
-        _table: 'npmPackage',
-        jsonData: JSON.stringify(pkgJson),
-      });
-      // console.dir(res, { depth: null });
-      console.log('Success', i, packageNames[i]);
-      // await wait(70);
-    } catch (error) {
-      console.log('Error Inserting', i, packageNames[i]);
-      // const parsedPkgJson = parsePackageJson(pkgJson);
-      // console.dir(parsedPkgJson, { depth: null });
-      console.dir(error, { depth: null });
-      // console.error(error);
-    }
-  } catch (error) {
+  // try {
+  const res = await insertJson(dgraphClient, {
+    _id: packageNames[i],
+    _table: 'npmPackage',
+    jsonData: JSON.stringify(pkgJson),
+  }).catch((error) => {
     console.log('Error fetching', i, packageNames[i]);
     console.error(error);
-  }
+  });
+  // console.dir(res, { depth: null });
+  console.log('Success', i, packageNames[i]);
+  // await wait(70);
+  // } catch (error) {
+  //   console.log('Error Inserting', i, packageNames[i]);
+  //   // const parsedPkgJson = parsePackageJson(pkgJson);
+  //   // console.dir(parsedPkgJson, { depth: null });
+  //   console.dir(error, { depth: null });
+  //   // console.error(error);
+  // }
+  // } catch (error) {
+  //   console.log('Error fetching', i, packageNames[i]);
+  //   console.error(error);
+  // }
   dgraphClientStub.close();
+  // } catch (error) {
+  //   console.log('Error in writeNpmPackageNamesToDatabaseSub');
+  //   console.error(error);
+  // }
 }
 
 async function writeNpmPackageNamesToDatabase() {
@@ -49,7 +65,7 @@ async function writeNpmPackageNamesToDatabase() {
     folderPath: ['..', '..', '..', '..', 'datasets'],
   });
 
-  const index = 298000;
+  const index = 67091;
   // const index = packageNames.findIndex(
   //   (packageName) => packageName === "@wind-webcli/core",
   // );
@@ -57,11 +73,13 @@ async function writeNpmPackageNamesToDatabase() {
   // for (let i = index; i < 953; i++) {
   let loopCounter = 0;
   for (let i = index; i < packageNames.length; i++) {
+    console.log({ loopCounter, i });
+    // await writeNpmPackageNamesToDatabaseSub(packageNames, i);
     writeNpmPackageNamesToDatabaseSub(packageNames, i);
 
-    if (loopCounter === 50) {
-      console.log('Waiting for 3 seconds');
-      await wait(3000);
+    if (loopCounter >= 10) {
+      console.log('Waiting for 2 seconds');
+      await wait(2000);
       loopCounter = 0;
     }
 
@@ -70,3 +88,16 @@ async function writeNpmPackageNamesToDatabase() {
 }
 
 // writeNpmPackageNamesToDatabase();
+
+// setInterval(() => {
+//   console.table(
+//     Object.entries(getHeapStatistics()).map(([k, v]) => ({
+//       k,
+//       v: v / 1024 / 1024,
+//     }))
+//   );
+// }, 1000);
+
+// setTimeout(function () {
+//   log(); // logs out active handles that are keeping node running
+// }, 200);
